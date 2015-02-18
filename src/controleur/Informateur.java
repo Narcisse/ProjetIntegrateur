6e30f9a4d4367b85886d97939d92641d6271edca
Narcisse <desrosch@gmail.com>
2015-02-18 08:02:05 -0500

    Create Informateur.java

---
+++ src/controleur/Informateur.java
@@ -0,0 +1,11 @@
+package controleur;
+import java.time.*;
+/*
+*	Christo
+*	Une classe qui peut être utilisée pour accéder à diverses informations
+*	comme la date et l'heure, la dimension de l'écran etc.
+*/
+
+public class Informateur {
+	
+}
