a10245d340917e2db776fa6c41b18f1d18bc44b2
Narcisse <desrosch@gmail.com>
2015-02-19 12:49:57 -0500

    Commentaires ajoutés

--- src/vue/PanneauxMenus/PanelOptions.java
+++ src/vue/PanneauxMenus/PanelOptions.java
@@ -2,7 +2,7 @@ package vue.PanneauxMenus;
 
 /**
  * Loic Grant-Steinhardt
- *
+ * ReCommenté par Christo Mondor
  */
 
 import controleur.DonneesUtiles;
@@ -19,8 +19,9 @@ public class PanelOptions extends PanelElder {
     private JButton bSon, bVideo, bControl, bOK;
     private JPanel panOptions;
     private JLabel labSpace;
-    private CardLayout cLayout;
-    private Image img;
+    // initialisés avec null pour pouvoir traiter le cas du constrcuteur vide
+    private CardLayout cLayout = null;
+    private Image img = null;
 
     public PanelOptions(Image img, CardLayout cLayout) {
         super();
@@ -42,6 +43,7 @@ public class PanelOptions extends PanelElder {
     // Methodes spécifiques
 
     public void initComponents() {
+        // Panneau de 5 par 1 en gridLayout pour offrir une liste d'options
         panOptions = new JPanel(new GridLayout(5, 1, 10, 10));
         panOptions.setBackground(new Color(0, 0, 0, 0));
 
@@ -61,9 +63,15 @@ public class PanelOptions extends PanelElder {
     }
 
     public void initListeners() {
+        // Lorsqu'on initie le panneau avec un card layout cette fonction s'occupe de changer la carte affichée
+        // Lorsqu'on initie le panneau de facon vide, cette fonction dispose du cadre qui contient ce panneau
         bOK.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
-                cLayout.show(getParent(), PanMenuPrincipal.CARTE_MENU_PRINCIPALE);
+                if (!cLayout == null){
+                    cLayout.show(getParent(), PanMenuPrincipal.CARTE_MENU_PRINCIPALE);
+                }else{
+                    super.disposerDuCadreDuPanneau(unPanneau);
+                }
             }
         });
 
