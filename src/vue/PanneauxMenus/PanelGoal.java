5aea7db0acffe042a58bf8fa5a44ce2bf4acd442
Narcisse <desrosch@gmail.com>
2015-02-19 13:05:48 -0500

    Commentaires ajoutés

--- src/vue/PanneauxMenus/PanelGoal.java
+++ src/vue/PanneauxMenus/PanelGoal.java
@@ -2,6 +2,7 @@ package vue.PanneauxMenus;
 
 /**
  * Loic Grant-Steinhardt
+ * ReCommenté par Christo Mondor
  */
 import javax.swing.*;
 import java.awt.*;
@@ -27,13 +28,22 @@ public class PanelGoal extends PanelElder {
     // Methodes Specifiques
 
     public void initComponents() {
+        /*
+         La disposition est simple, un panneau de fond (this) qui contient
+         deux autres panneau dans un gridlayout de 2 par 1
+        
+         Le panHaut contient deux label qui affiche un texte pré-définis dans le
+         code ou dans un fichier texte, selon le stade de développement
+         */
         this.cePanneau = this;
         panGoal = new JPanel(new GridLayout(2, 1, 10, 10));
         panHaut = new JPanel(new GridLayout(2, 1, 10, 10));
         panBas = new JPanel();
+        // Création d'une bordure pour le panneau qui englobe le tout (panGoal)
         Border noir = BorderFactory.createLineBorder(Color.black);
         Border titre = BorderFactory.createTitledBorder(noir, "Objectifs");
         panGoal.setBorder(titre);
+        // Création des labels qui contiennent les objectifs et le texte général
         labGoal1 = new JLabel("Vous devez repousser les vagues d'ennemis le plus longtemps possible.");
         labGoal2 = new JLabel("Bonne chance, vous en aurez besoin...");
         bOK = new JButton("     OK     ");
@@ -52,6 +62,10 @@ public class PanelGoal extends PanelElder {
             }
         });
     }
+    /*
+    Cette fonction permet de disposer d'un window donc d'un JFrame, un Jdialog etc. dans n'importe quelle
+    situation et dans n'importe quelle partie du code
+    */
     @Override
     public void disposerDuCadreDuPanneau(JPanel unPanneau) {
         super.disposerDuCadreDuPanneau(cePanneau);
