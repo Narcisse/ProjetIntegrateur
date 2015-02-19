a6e0d773198876ba1a5d26f669ebd1d574a6b694
Narcisse <desrosch@gmail.com>
2015-02-19 13:12:33 -0500

    Commentaires ajoutés

--- src/vue/PanneauxMenus/PanelHelp.java
+++ src/vue/PanneauxMenus/PanelHelp.java
@@ -2,6 +2,9 @@ package vue.PanneauxMenus;
 
 /**
  * Loic Grant-Steinhardt
+ * Un panneau pour afficher une aide contextuel pour les
+ * touches du jeu
+ * ReCommenté par Christo Mondor
  */
 import javax.swing.*;
 import java.awt.*;
@@ -28,6 +31,14 @@ public class PanelHelp extends PanelElder {
     // Methodes spécifiques
 
     public void initComponents() {
+        /*
+         La disposition est simple, un panneau de fond (this) qui contient
+         deux autres panneau dans un gridlayout de 2 par 1
+        
+         Le panHaut contient un textArea qui affiche un texte pré-définis pour
+         rappeler les commandes à l'utilisateur. Le texte peut être lu dans un
+         fichier, dans une version du programme à venir
+         */
         this.cePanneau = this;
         panHelp = new JPanel(new GridLayout(2, 1, 10, 10));
         panHaut = new JPanel(new GridLayout(1, 1, 10, 10));
@@ -58,6 +69,10 @@ public class PanelHelp extends PanelElder {
             }
         });
     }
+    /*
+    Cette fonction permet de disposer d'un window donc d'un JFrame, un Jdialog etc. dans n'importe quelle
+    situation et dans n'importe quelle partie du code
+    */
     @Override
     public void disposerDuCadreDuPanneau(JPanel unPanneau) {
         super.disposerDuCadreDuPanneau(unPanneau);
