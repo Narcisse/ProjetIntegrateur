f3d966cd0f0828be9be1dacdd902e7ad2731e2d4
maurisPU01 <mauriciofipi@hotmail.com>
2015-02-26 12:42:45 -0500

    changement mineur

--- src/controleur/Informateur.java
+++ src/controleur/Informateur.java
@@ -9,6 +9,8 @@ import javax.swing.ImageIcon;
 *	Christo
 *	Une classe qui peut être utilisée pour accéder à diverses informations
 *	comme la date et l'heure, la dimension de l'écran etc.
+
+        sa fonctionne
 */
 
 public class Informateur {
