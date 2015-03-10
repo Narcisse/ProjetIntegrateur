726e3366bc61bca65879b161e09b0fccdeec5775
maurisPU01 <mauriciofipi@hotmail.com>
2015-03-10 15:17:22 -0400

    J'ai modifier la calsse pour qu'elle "extends" de Element et j'ai effacé les attributs et les méthodes qui se trouve dans la classe element.

--- src/modele/Unite.java
+++ src/modele/Unite.java
@@ -6,46 +6,27 @@
 
 package modele;
 
-public abstract class Unite{
+public abstract class Unite extends Element{
 
     // Donnee membres
     // Attribut
-    private String nom;
-    private int vie;
     private int dps;
-    private int orNecessaire;
     private int nourritureNecessaire;
 
-    // Methodes specifiques
-    public abstract String toString();
 	
     // Mutateurs
-    public void setVie(int unMontant) {
-        this.vie = unMontant;
-    }
     public void setDps(int unMontant) {
         this.dps = unMontant;
-    }
-    public void setOrNecessaire(int unMontant) {
-        this.orNecessaire = unMontant;
     }
     public void setNourritureNecessaire(int unMontant) {
         this.nourritureNecessaire = unMontant;
     }
 
     // Accesseurs
-    public String getNom() {
-        return this.nom;
-    }
-    public int getVie() {
-        return this.vie;
-    }
     public int getDps() {
         return this.dps;
     }
-    public int getOrNecessaire() {
-        return this.orNecessaire;
-    }
+
     public int getNourritureNecessaire() {
         return this.nourritureNecessaire;
     }
