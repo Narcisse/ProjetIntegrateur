28120a9c305ea6c1eb4ccaf5afa388ca197a24d1
maurisPU01 <mauriciofipi@hotmail.com>
2015-03-10 15:06:36 -0400

    J'ai crée une méhtode qui regroupe les attributs et les methodes en commun des classes Unite et Batiment entre autre nom, vie, orNecessaire, getNom(), getOrNecessaire() etc.

---
+++ src/modele/Element.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+/**
+ *
+ * @author Mauricio
+ * La classe Elements est une classe mere qui regroupe les element en commun des 
+ * classes Unite et Batiment
+ */
+public class Element {
+    
+}
