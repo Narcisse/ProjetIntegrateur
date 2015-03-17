5132cd549fc0afae23e8779528988f6a9c701c99
dpx2k1 <sk8_7506@hotmail.com>
2015-03-17 01:28:22 -0400

    Selection box rts style +ArrayList de personnage(bug)

--- src/vue/PanneauxInterface/PlancheDeJeu.java
+++ src/vue/PanneauxInterface/PlancheDeJeu.java
@@ -2,6 +2,7 @@ package vue.PanneauxInterface;
 
 import controleur.Camera;
 import controleur.ControlleurPersonnage;
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
@@ -12,6 +13,7 @@ import org.newdawn.slick.Input;
 import org.newdawn.slick.MouseListener;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Transform;
 import vue.Jeu.Carte;
 import vue.Jeu.Joueur;
 
@@ -34,8 +36,16 @@ public class PlancheDeJeu extends BasicGame {
     // Curseur
     int xCurseur = -1000;
     int yCurseur = -1000;
-
+    //Utilisé pour savoir a quelle endroit la souris est cliqué
+    int xPressed = 0;
+    int yPressed = 0;
+    //Le delta de la position initiale de la souris et de sa position finale
+    int deltaX = 0;
+    int deltaY = 0;
+    //Rectangle contruit par la souris
     private Rectangle rect;
+    //ArrayList de personnage selectionné
+    private ArrayList lstSelection = new ArrayList();
 
     // *************************************************************************
     // Constructeur
@@ -65,16 +75,19 @@ public class PlancheDeJeu extends BasicGame {
         container.getInput().addMouseListener(ecoPerso);
     }
 
+    //Methode qui rennder la frame
     public void render(GameContainer container, Graphics g) throws SlickException {
         this.camera.place(container, g);
         this.cartePrincipale.renderArrierePlan();
         this.personnage.render(g);
         this.cartePrincipale.renderAvantPlan();
         if (rect != null) {
+            g.setColor(new Color(255, 255, 255, 100));
             g.drawRect(rect.getX(), rect.getY(), rect.getWidth(), rect.getHeight());
         }
     }
 
+    //Methode qui actualise la frame (selon le fps)
     public void update(GameContainer container, int delta) throws SlickException {
         this.personnage.update(delta);
         this.camera.update(container);
@@ -102,10 +115,53 @@ public class PlancheDeJeu extends BasicGame {
         }
     }
 
-    public void mouseDragged(int oldx, int oldy, int newx, int newy){
-        Input input = container.getInput();
-        rect = new Rectangle(oldx, oldy, newx - oldx, newy - oldy);
+    //*****************************************************************
+    //Methode qui trouve la position de la souris au moment où on clique
+    public void mousePressed(int button, int x, int y) {
+        if (button == 0) {
+            xPressed = (int) (x + (camera.getX() - container.getWidth() / 2));
+            yPressed = (int) (y + (camera.getY() - container.getHeight() / 2));
+        }
     }
+
+    //Methode qui calcule la nouvelle position de la souris lorsqu'elle est cliqué
+    public void mouseDragged(int oldx, int oldy, int newx, int newy) {
+
+        //newx/newy sont les positions en temps réels de la souris lorsque cliqué
+        //xPressed/yPressed est le clique initiale de la souris
+        deltaX = (int) ((newx - xPressed) + (camera.getX() - container.getWidth() / 2));
+        deltaY = (int) ((newy - yPressed) + (camera.getY() - container.getHeight() / 2));
+
+        //set le rectangle grace aux variables calculées dans la methode
+        rect = new Rectangle(xPressed, yPressed, deltaX, deltaY);
+
+        //Si les personnages se trouvent dans le rectangle construit il sont ajoutés à la liste
+        //HautGauche vers BasDroit
+        if (deltaX > 1 && deltaY > 1) {
+            if (rect.getX() <= personnage.getX() && rect.getY() <= personnage.getY()) {
+                //Ajoute à la liste mais trop (c'est peut-etre pas grave though) -> 
+                //On devrait faire un boolean dans joueur qui vien canceler l'ajout s'il n'est pas en mouvement?
+                lstSelection.add(personnage);
+                System.out.println(lstSelection.size());
+            }
+        } //BasDroit vers HautGauche
+        else if (deltaX < 1 && deltaY < 1) {
+            if (rect.getX() >= personnage.getX() && rect.getY() >= personnage.getY()) {
+                personnage.selection();
+            }
+        } //HautDroit vers BasGauche
+        else if (deltaX < 1 && deltaY > 1) {
+            if (rect.getX() >= personnage.getX() && rect.getY() <= personnage.getY()) {
+                personnage.selection();
+            }
+        } //BasGauche vers HautDroit
+        else if (deltaX > 1 && deltaY < 1) {
+            if (rect.getX() <= personnage.getX() && rect.getY() >= personnage.getY()) {
+                personnage.selection();
+            }
+        }
+    }
+    //*****************************************************************
 
     public void testLog() {
         System.out.println("x2: " + ((int) personnage.getX() - 32));
