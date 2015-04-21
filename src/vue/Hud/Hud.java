7cb1d3850116c711507bc5afd55a0b79f547d69c
ketchups <south_of_heaven2@hotmail.com>
2015-04-21 12:46:55 -0400

    Update Hud.java

--- src/vue/Hud/Hud.java
+++ src/vue/Hud/Hud.java
@@ -13,17 +13,15 @@ import org.newdawn.slick.gui.AbstractComponent;
 import org.newdawn.slick.gui.GUIContext;
 import modele.Entrepot;
 
-
 /**
  *
  * @author Christopher Desrosiers Mondor
  */
-public class Hud extends AbstractComponent implements MouseListener{
-    
+public class Hud extends AbstractComponent implements MouseListener {
+
     private Image paneauAction;
     public static int positionXPaneauAction, positionYPaneauAction;
     public static int tailleImagePaneauAction;
-   
 
     // Composants du Hud
     private Image imgBois; // image bois
@@ -37,9 +35,8 @@ public class Hud extends AbstractComponent implements MouseListener{
     // Ressources extérieures
     private Camera camera;
     private GameContainer container;
-    
-    private Entrepot banque;
 
+    private Entrepot banque;
 
     public Hud(GUIContext container, Camera uneCamera, GameContainer unContainer) throws SlickException {
         super(container);
@@ -49,47 +46,44 @@ public class Hud extends AbstractComponent implements MouseListener{
     }
 
     public void init() throws SlickException {
-        banque = new Entrepot(50,40,30);
+        banque = new Entrepot(50, 40, 30);
         this.imgNourriture = new Image("images//Icone//nourriture.png");
         this.imgOr = new Image("images//Icone//or.png");
         this.imgBois = new Image("images//Icone//bois.png");
         this.paneauAction = new Image("images//romanStone.jpg");
     }
-    
+
     public void render(Graphics g) {
-        /*g.resetTransform();
+        //cette méthode empeche ce qui est dessiné de bouger dans l'écran  
+        g.resetTransform();
         //PANEAU ACTION JOUEUR 
-        int hauteurFrameY= container.getHeight();
-        int largeurFrameX= container.getWidth();
-        int tailleImageX = largeurFrameX/4;
-        int tailleImageY = hauteurFrameY/4;
-        if(tailleImageX < tailleImageY){
+        int hauteurFrameY = container.getHeight();
+        int largeurFrameX = container.getWidth();
+        int tailleImageX = largeurFrameX / 4;
+        int tailleImageY = hauteurFrameY / 4;
+        if (tailleImageX < tailleImageY) {
             tailleImagePaneauAction = tailleImageX;
-        }else{
+        } else {
             tailleImagePaneauAction = tailleImageY;
-        }  
+        }
         positionXPaneauAction = largeurFrameX - tailleImagePaneauAction - 10;
         positionYPaneauAction = hauteurFrameY - tailleImagePaneauAction - 10;
-        
+
         paneauAction.draw(positionXPaneauAction, positionYPaneauAction, tailleImagePaneauAction, tailleImagePaneauAction);
-       */
-      //cette méthode empeche ce qui est dessiné de bouger dans l'écran  
-      g.resetTransform();
-      //On dessine l'icone de l'or avec  sa quantité
-      g.drawImage(imgOr, 20, 20);
-      g.drawString(": " + banque.getOr(), 60, 24);
-      //On dessine l'icone de nourriture avec  sa quantité
-      g.drawImage(imgNourriture, 120, 20);
-      g.drawString(": "+ banque.getNourriture(), 160, 24);
-      //On dessine l'icone de bois avec  sa quantité
-      g.drawImage(imgBois, 220, 20);
-      g.drawString(": "+ banque.getBois(), 260, 24);
+
+        //On dessine l'icone de l'or avec  sa quantité
+        //  g.drawImage(imgOr, 20, 20);
+        g.drawString(": " + banque.getOr(), 60, 24);
+        //On dessine l'icone de nourriture avec  sa quantité
+        //g.drawImage(imgNourriture, 120, 20);
+        g.drawString(": " + banque.getNourriture(), 160, 24);
+        //On dessine l'icone de bois avec  sa quantité
+        //  g.drawImage(imgBois, 220, 20);
+        g.drawString(": " + banque.getBois(), 260, 24);
     }
-    
-    
+
     // *************************************************************************
     // Ecouteurs
-
     @Override
     public void mouseWheelMoved(int i) {
     }
@@ -120,7 +114,7 @@ public class Hud extends AbstractComponent implements MouseListener{
 
     @Override
     public boolean isAcceptingInput() {
-            return true;
+        return true;
     }
 
     @Override
@@ -159,3 +153,4 @@ public class Hud extends AbstractComponent implements MouseListener{
         return 0;
     }
 }
+
