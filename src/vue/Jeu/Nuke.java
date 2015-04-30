cf91c315d48b06296d9f4c46a41f05ae58703c80
Narcisse <desrosch@gmail.com>
2015-04-30 10:56:28 -0400

    Changement Atttribut, soulier et nuke, il faut implementer un render dans les classes filles.

--- src/vue/Jeu/Nuke.java
+++ src/vue/Jeu/Nuke.java
@@ -1,20 +1,35 @@
 package vue.Jeu;
 
 import java.util.ArrayList;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import vue.Jeu.Ennemi;
 
-
 public class Nuke extends Attribut {
-    Image imgAttribut;;
+
+    Image imgAttribut;
     double rare;
-    
-    public Nuke() throws SlickException{this.imgAttribut = new Image("data/sprites/objet/Nuke.png");
-}
-    public void action(){}
-    public void action(Joueur unJoueur){}
-    public void action(Ennemi unEnnemi){unEnnemi.removeHP(unEnnemi.getHP());}
-    public void action(ArrayList uneListe){}
-    
+
+    public Nuke() throws SlickException {
+        this.imgAttribut = new Image("data/sprites/objet/Nuke.png");
+    }
+
+    public void action() {
+    }
+
+    public void action(Joueur unJoueur) {
+    }
+
+    public void action(Ennemi unEnnemi) {
+        unEnnemi.removeHP(unEnnemi.getHP());
+    }
+
+    public void action(ArrayList uneListe) {
+    }
+
+    @Override
+    public void render(Graphics g) throws SlickException {
+    }
+
 }
