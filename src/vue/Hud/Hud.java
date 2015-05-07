f028209cb311d96c4f08182f9aeeaa2c5a322aa4
ketchups <south_of_heaven2@hotmail.com>
2015-05-07 12:17:45 -0400

    MAJ hud

--- src/vue/Hud/Hud.java
+++ src/vue/Hud/Hud.java
@@ -11,146 +11,113 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.gui.AbstractComponent;
 import org.newdawn.slick.gui.GUIContext;
-import modele.Entrepot;
+import vue.ElementsPrincipauxDuJeu.Joueur;
+import vue.GameSates.Game;
 
 /**
  *
  * @author Christopher Desrosiers Mondor
  */
-public class Hud extends AbstractComponent implements MouseListener {
+public class Hud extends AbstractComponent {
 
-    private Image paneauAction;
-    public static int positionXPaneauAction, positionYPaneauAction;
-    public static int tailleImagePaneauAction;
+    //Objets
+    private Joueur joueur;
+    private Game game;
 
     // Composants du Hud
-    private Image imgBois; // image bois
-    private Image imgOr; //  image Or
-    private Image imgNourriture; // image nourriture
-    // Bas
-    private Image actionBar; // Doit etre fait **
-    private Image miniMap; // Doit etre fait **
-    private Image informationBar; // Doit etre fait **
+    private Image barreVie;
+    private Image actionBar;    
+    private Image attributImg;
 
     // Ressources extérieures
     private Camera camera;
     private GameContainer container;
 
-    private Entrepot banque;
-
-    public Hud(GUIContext container, Camera uneCamera, GameContainer unContainer) throws SlickException {
+    //Variables pour barreVie
+    private final int P_BAR_X = 10;
+    private final int P_BAR_Y = 10;
+    private final int BAR_X = 84 + P_BAR_X;
+    private final int LIFE_BAR_Y = 4 + P_BAR_Y;
+    private final int BAR_WIDTH = 80;
+    private final int BAR_HEIGHT = 16;
+    private final Color LIFE_COLOR = new Color(255, 0, 0);
+    
+    public Hud(GUIContext container, Camera uneCamera, GameContainer unContainer, Joueur joueur) throws SlickException {
         super(container);
         this.container = unContainer;
         this.camera = uneCamera;
+        this.joueur = joueur;
         init();
     }
-
+    
     public void init() throws SlickException {
-        banque = new Entrepot(50, 40, 30);
-        this.imgNourriture = new Image("images//Icone//hamIcon.png");
-        this.imgOr = new Image("images//Icone//goldicon.png");
-        this.imgBois = new Image("images//Icone//bois.png");
-        this.paneauAction = new Image("images//romanStone.jpg");
+        this.barreVie = new Image("data//Hud//barreVie.png");
+        this.attributImg = new Image("images//chat.jpg");
+        this.game = new Game();
     }
-
+    
     public void render(Graphics g) {
         //cette méthode empeche ce qui est dessiné de bouger dans l'écran  
         g.resetTransform();
-        //PANEAU ACTION JOUEUR 
-        int hauteurFrameY = container.getHeight();
-        int largeurFrameX = container.getWidth();
-        int tailleImageX = largeurFrameX / 4;
-        int tailleImageY = hauteurFrameY / 4;
-        if (tailleImageX < tailleImageY) {
-            tailleImagePaneauAction = tailleImageX;
-        } else {
-            tailleImagePaneauAction = tailleImageY;
-        }
-        positionXPaneauAction = largeurFrameX - tailleImagePaneauAction - 10;
-        positionYPaneauAction = hauteurFrameY - tailleImagePaneauAction - 10;
 
-        paneauAction.draw(positionXPaneauAction, positionYPaneauAction, tailleImagePaneauAction, tailleImagePaneauAction);
-
-        //On dessine l'icone de l'or avec  sa quantité
-        //  g.drawImage(imgOr, 20, 20);
-        g.drawString(": " + banque.getOr(), 60, 24);
-        //On dessine l'icone de nourriture avec  sa quantité
-        //g.drawImage(imgNourriture, 120, 20);
-        g.drawString(": " + banque.getNourriture(), 160, 24);
-        //On dessine l'icone de bois avec  sa quantité
-        //  g.drawImage(imgBois, 220, 20);
-        g.drawString(": " + banque.getBois(), 260, 24);
+        //On dessine la barre de vie en rouge et on la draw à l'endroit voulu
+        g.setColor(LIFE_COLOR);
+        //On remplit la barre
+        g.fillRect(BAR_X, LIFE_BAR_Y, (float)(joueur.getHP() / 100.0) * BAR_WIDTH - 40, BAR_HEIGHT);
+        g.drawImage(this.barreVie, P_BAR_X, P_BAR_Y);
+        g.drawImage(this.attributImg.getScaledCopy(40, 40), 22, 20);
+        g.setColor(new Color(255, 255, 255));
+        g.drawString("Score : " + game.getScore(), 1315, 15);
     }
 
     // *************************************************************************
     // Ecouteurs
-    @Override
-    public void mouseWheelMoved(int i) {
+    public void setImageAttribut(Image attributImg) {
+        this.attributImg = attributImg;
     }
-
-    @Override
-    public void mouseClicked(int i, int i1, int i2, int i3) {
-    }
-
-    @Override
-    public void mousePressed(int button, int i1, int i2) {
-    }
-
-    @Override
-    public void mouseReleased(int i, int i1, int i2) {
-    }
-
-    @Override
-    public void mouseMoved(int i, int i1, int i2, int i3) {
-    }
-
-    @Override
-    public void mouseDragged(int i, int i1, int i2, int i3) {
-    }
-
+    
     @Override
     public void setInput(Input input) {
     }
-
+    
     @Override
     public boolean isAcceptingInput() {
         return true;
     }
-
+    
     @Override
     public void inputEnded() {
     }
-
+    
     @Override
     public void inputStarted() {
     }
-
+    
     @Override
     public void render(GUIContext guic, Graphics grphcs) throws SlickException {
     }
-
+    
     @Override
     public void setLocation(int i, int i1) {
     }
-
+    
     @Override
     public int getX() {
         return 0;
     }
-
+    
     @Override
     public int getY() {
         return 0;
     }
-
+    
     @Override
     public int getWidth() {
         return 0;
     }
-
+    
     @Override
     public int getHeight() {
         return 0;
     }
 }
-
