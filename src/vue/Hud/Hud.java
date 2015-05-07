22cb6d2eb6ac0fd0502f6fdcc049f73dcee4d0b3
ketchups <south_of_heaven2@hotmail.com>
2015-05-07 13:50:40 -0400

    Update Hud.java

--- src/vue/Hud/Hud.java
+++ src/vue/Hud/Hud.java
@@ -38,6 +38,7 @@ public class Hud extends AbstractComponent {
     private final int P_BAR_Y = 10;
     private final int BAR_X = 84 + P_BAR_X;
     private final int LIFE_BAR_Y = 4 + P_BAR_Y;
+    private final int ARMOR_BAR_Y = 24 + P_BAR_Y;
     private final int BAR_WIDTH = 80;
     private final int BAR_HEIGHT = 16;
     private final Color LIFE_COLOR = new Color(255, 0, 0);
@@ -60,10 +61,14 @@ public class Hud extends AbstractComponent {
         //cette méthode empeche ce qui est dessiné de bouger dans l'écran  
         g.resetTransform();
 
-        //On dessine la barre de vie en rouge et on la draw à l'endroit voulu
+       //On dessine la barre de vie en rouge et on la draw à l'endroit voulu
         g.setColor(LIFE_COLOR);
         //On remplit la barre
-        g.fillRect(BAR_X, LIFE_BAR_Y, (float)(joueur.getHP() / 100.0) * BAR_WIDTH - 40, BAR_HEIGHT);
+        g.fillRect(BAR_X , LIFE_BAR_Y, (float)(joueur.getVie() / joueur.getVieMax()) * BAR_WIDTH, BAR_HEIGHT);
+        //Barre d'armure
+        g.setColor(Color.blue);
+        g.fillRect(BAR_X ,ARMOR_BAR_Y, (float)(joueur.getArmure() / joueur.getArmureMax()) * BAR_WIDTH , BAR_HEIGHT);
+        //On dessine la barre de vie
         g.drawImage(this.barreVie, P_BAR_X, P_BAR_Y);
         g.drawImage(this.attributImg.getScaledCopy(40, 40), 22, 20);
         g.setColor(new Color(255, 255, 255));
